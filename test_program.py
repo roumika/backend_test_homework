7cea712b79c68ee0539134adb0a81e9999947e1f
roumika <115934309+roumika@users.noreply.github.com>
2022-10-16 17:29:07 +0300

    Update test_program.py

--- test_program.py
+++ test_program.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
 files_list = ['program.py', 'readme.md']
-
+ 
 
 def test_program():
     for filename in files_list:
